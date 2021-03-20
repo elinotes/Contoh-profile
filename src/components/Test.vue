a255b111ff7eec371977a10480781d60cafede04
elinotes <78309546+elinotes@users.noreply.github.com>
2021-03-20 20:54:49 +0700

    Update Test.vue
    ubah link kontak

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -33,7 +33,7 @@
                 <!-- image for mobile -->
                 <img class="block lg:hidden object-cover object-top rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" src="../assets/20200429_145620.png"/>
                     
-                <h1 class="text-3xl font-bold text-gray-900 lg:pt-0 ">Good to see you , I'am Novan..</h1>
+                <h1 class="text-3xl font-bold text-gray-900 lg:pt-0 ">Good to see you , I'am elinotes</h1>
                     <div class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-teal-500 opacity-25"></div>   
                 <p class="text-gray-900 pt-4 text-base font-bold flex items-center justify-center lg:justify-start"><svg class="h-4 fill-current text-teal-700 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9 12H1v6a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-6h-8v2H9v-2zm0-1H0V5c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h4a2 2 0 0 1 2 2v6h-9V9H9v2zm3-8V2H8v1h4z"/></svg>Junior FrontEnd Engineer</p>
                 <p class="pt-2 text-gray-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start"><svg class="h-4 fill-current text-teal-700 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm7.75-8a8.01 8.01 0 0 0 0-4h-3.82a28.81 28.81 0 0 1 0 4h3.82zm-.82 2h-3.22a14.44 14.44 0 0 1-.95 3.51A8.03 8.03 0 0 0 16.93 14zm-8.85-2h3.84a24.61 24.61 0 0 0 0-4H8.08a24.61 24.61 0 0 0 0 4zm.25 2c.41 2.4 1.13 4 1.67 4s1.26-1.6 1.67-4H8.33zm-6.08-2h3.82a28.81 28.81 0 0 1 0-4H2.25a8.01 8.01 0 0 0 0 4zm.82 2a8.03 8.03 0 0 0 4.17 3.51c-.42-.96-.74-2.16-.95-3.51H3.07zm13.86-8a8.03 8.03 0 0 0-4.17-3.51c.42.96.74 2.16.95 3.51h3.22zm-8.6 0h3.34c-.41-2.4-1.13-4-1.67-4S8.74 3.6 8.33 6zM3.07 6h3.22c.2-1.35.53-2.55.95-3.51A8.03 8.03 0 0 0 3.07 6z"/></svg> Indonesian -7.967348° N, 112.650497° W</p>
@@ -52,12 +52,12 @@
                     </div>
 
                     <div class="pt-3 pb-8 flex justify-around border-t space-x-10 mx-6">
-                        <button class="flex-1 bg-teal-700 hover:bg-teal-900 text-white font-bold py-2 px-4 rounded-full" onclick="window.location.href = 'https://wa.me/628971055226?text=I%20interested%20with%20your%20work';">
-                        Chat me
+                        <button class="flex-1 bg-teal-700 hover:bg-teal-900 text-white font-bold py-2 px-4 rounded-full" onclick="window.location.href = 'https://www.elinotes.com/';">
+                        Elinotes
                         </button> 
 
-                        <button class="flex-1 bg-teal-700 hover:bg-teal-900 text-white font-bold py-2 px-4 rounded-full" onclick="window.location.href = 'https://drive.google.com/file/d/1ELuelT4aqly5d_JIkplGX0H5RD_7LPFA/view?usp=sharing';">
-                        My Resume
+                        <button class="flex-1 bg-teal-700 hover:bg-teal-900 text-white font-bold py-2 px-4 rounded-full" onclick="window.location.href = 'https://www.elinotes.com/p/contact.html';">
+                        contact me
                         </button> 
                     </div>
                     
